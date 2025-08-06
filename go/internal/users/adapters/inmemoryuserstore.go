a49f8cea5614b2dac3bba8072e4d7bb36068fdb0
Joshua Hughes <josh.hughes@live.co.uk>
2025-08-07 00:07:33 +0100

    fix: Added mutex to user store

--- go/internal/users/adapters/inmemoryuserstore.go
+++ go/internal/users/adapters/inmemoryuserstore.go
@@ -2,17 +2,22 @@ package adapters
 
 import (
 	"eaglebank/internal/users"
+	"sync"
 )
 
 type InMemoryUserStore struct {
+	mu    sync.RWMutex
 	store map[users.UserID]users.User
 }
 
-func NewInMemoryUserStore() InMemoryUserStore {
-	return InMemoryUserStore{store: map[users.UserID]users.User{}}
+func NewInMemoryUserStore() *InMemoryUserStore {
+	return &InMemoryUserStore{store: map[users.UserID]users.User{}}
 }
 
-func (s InMemoryUserStore) Get(id users.UserID) (users.User, error) {
+func (s *InMemoryUserStore) Get(id users.UserID) (users.User, error) {
+	s.mu.RLock()
+	defer s.mu.RUnlock()
+
 	user, ok := s.store[id]
 	if !ok {
 		return users.User{}, users.ErrUserNotFound
@@ -20,12 +25,18 @@ func (s InMemoryUserStore) Get(id users.UserID) (users.User, error) {
 	return user, nil
 }
 
-func (s InMemoryUserStore) Put(user users.User) error {
+func (s *InMemoryUserStore) Put(user users.User) error {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
 	s.store[user.ID] = user
 	return nil
 }
 
-func (s InMemoryUserStore) Delete(id users.UserID) error {
+func (s *InMemoryUserStore) Delete(id users.UserID) error {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
 	delete(s.store, id)
 	return nil
 }
